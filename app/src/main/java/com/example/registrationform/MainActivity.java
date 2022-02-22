2a51d3532ab76e3ce446296df7ff9edc4f081568
Amina Khatun <aminaemcbd@gmail.com>
2022-02-23 00:48:22 +0600

    sadf

--- app/src/main/java/com/example/registrationform/MainActivity.java
+++ app/src/main/java/com/example/registrationform/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.registrationform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        String[] location = {"Dhanmondi", "Shamoly", "gulshan"};
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.custom_spinner,location );
+         spinner.setAdapter(adapter);
     }
 }
