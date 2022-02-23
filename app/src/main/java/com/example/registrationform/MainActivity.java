72a420aaf796594e6d8cc0f7fdcfaa018fce65a6
unknown <Student@NHDL3-S6.nhd.local>
2022-02-23 10:25:37 +0600

    gridview

--- app/src/main/java/com/example/registrationform/MainActivity.java
+++ app/src/main/java/com/example/registrationform/MainActivity.java
@@ -4,19 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 Spinner spinner;
+ListView listView;
+GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = findViewById(R.id.spinner);
+         spinner = findViewById(R.id.spinner);
 
-        String[] location = {"Dhanmondi", "Shamoly", "gulshan"};
+        String[] location = {"Dhanmondi", "Shamoly", "gulshan","savar" , "kollanpur"};
         ArrayAdapter adapter = new ArrayAdapter(this, R.layout.custom_spinner,location );
          spinner.setAdapter(adapter);
+
+
+
+         listView = findViewById(R.id.listview);
+         listView.setAdapter(adapter);
+
+
+         gridView = findViewById(R.id.gridview);
+         gridView.setAdapter(adapter);
     }
 }
